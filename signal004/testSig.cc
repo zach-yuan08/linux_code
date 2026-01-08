ab90eb66642839ac269bb14c8c2706fc588c9916
yjc <zach_yuan0823@outlook.com>
2026-01-08 21:43:46 +0800

    关于信号004

---
+++ signal004/testSig.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <functional>
+#include <vector>
+
+void handler(int signum)
+{
+    std::cout << "hello signal:" << signum << std::endl;
+    exit(0);
+}
+int main()
+{
+    struct sigaction act, oact;
+    act.sa_handler = handler;
+    
+    sigaction(SIGINT, &act, &oact);
+     
+    while (true)
+    {
+        std::cout << "hello world" << std :: endl;
+        sleep(1);
+    }
+    
+    return 0;
+}
