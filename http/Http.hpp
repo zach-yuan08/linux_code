d3f4a16ad76cb00b5518ad302e1905584144b254
yjc <zach_yuan0823@outlook.com>
2026-02-06 10:35:46 +0800

    关于http的GET/POST

--- http/Http.hpp
+++ http/Http.hpp
@@ -32,31 +32,46 @@ public:
     void ParseReqLine(std::string &reqline)
     {
         std::stringstream ss(reqline);
-        ss >> _method >> _uri >> _version;
+        ss >> _method >> _uri >> _version; // std::stringstream 的 >> 提取运算符遵循空白字符分割规则
     }
 
     bool Deserialize(std::string &reqstr)
     {
         // 1. 提取请求中的请求行
         std::string reqline;
-        bool res = Util::ReadOneLine(reqstr, &reqline, glinespace);
+        bool res = Util::ReadOneLine(reqstr, &reqline, glinespace); // 获取请求行
         LOG(LogLevel::DEBUG) << reqline;
 
         // 2. 对请求行反序列化
         ParseReqLine(reqline);
 
-        LOG(LogLevel::DEBUG) << "_method:" << _method;
-        LOG(LogLevel::DEBUG) << "_uri:" << _uri;
-        LOG(LogLevel::DEBUG) << "_version:" << _version;
-
         if (_uri == "/") // 如果访问的是根目录，返回首页
             _uri = webroot + _uri + homepage;
         else
             _uri = webroot + _uri;
+
+        LOG(LogLevel::DEBUG) << "_method:" << _method;
+        LOG(LogLevel::DEBUG) << "_uri:" << _uri;
+        LOG(LogLevel::DEBUG) << "_version:" << _version;
+        const std::string temp = "?";
+        auto pos = _uri.find(temp);
+        if (pos == std::string::npos)
+        {
+            return true;
+        }
+        // _uri: ./wwwroot/login
+        // username=zhangsan&password=123456
+        _args = _uri.substr(pos + temp.size());
+        _uri = _uri.substr(0, pos);
+        _is_interact = true;
+
+        // ./wwwroot/XXX.YYY
         return true;
     }
 
     std::string Uri() { return _uri; }
+    bool isInteract() { return _is_interact; }
+    std::string Args() { return _args; }
 
     ~HttpRequest()
     {
@@ -70,6 +85,9 @@ private:
     std::unordered_map<std::string, std::string> _headers;
     std::string _blankline;
     std::string _text;
+
+    std::string _args;
+    bool _is_interact;
 };
 
 class HttpResponse
@@ -105,6 +123,12 @@ public:
         case 200:
             _desc = "OK";
             break;
+        case 301:
+            _desc = "Moved Permanently";
+            break;
+        case 302:
+            _desc = "See Other";
+            break;
         case 404:
             _desc = "NotFound";
             break;
@@ -113,6 +137,7 @@ public:
         }
     }
 
+    // 设置报头
     void SetHeader(const std::string &key, const std::string &value)
     {
         auto iter = _headers.find(key);
@@ -121,6 +146,7 @@ public:
         _headers.insert(std::make_pair(key, value));
     }
 
+    // 提取文件后缀，返回对应文件类型
     std::string Uri2Suffix(const std::string &targetfile)
     {
         // ./wwwroot/a/b/c.html
@@ -148,20 +174,39 @@ public:
             LOG(LogLevel::DEBUG) << "用户请求: " << _targetfile << "忽略它";
             return false;
         }
+        if (_targetfile == "./wwwroot/redir_test")
+        {
+            SetCode(301);
+            SetHeader("Location", "https://www.qq.com/");
+            return true;
+        }
 
         int filesize = 0;
         bool res = Util::ReadFileContent(_targetfile, &_text);
         if (!res)
         {
+            // 1. 清空响应体缓冲区，避免残留旧数据
             _text = "";
+            // 2. 打印警告日志：记录客户端请求的文件不存在
             LOG(LogLevel::WARNING) << "client want get : " << _targetfile << " but not found";
+            // 3. 设置HTTP状态码为404，标识资源未找到
             SetCode(404);
+            // 4. 重新指定目标文件为服务器内置的404页面
             _targetfile = webroot + page_404;
+            // 5. 获取404页面的文件大小，用于设置响应头
             filesize = Util::FileSize(_targetfile);
+            // 6. 读取404页面的HTML内容，写入响应体
             Util::ReadFileContent(_targetfile, &_text);
+            // 7. 根据文件后缀解析MIME类型，用于告知浏览器数据格式
             std::string suffix = Uri2Suffix(_targetfile);
+            // 8. 设置响应头：告诉浏览器响应体的内容类型
             SetHeader("Content-Type", suffix);
+            // 9. 设置响应头：告诉浏览器响应体的字节长度
             SetHeader("Content-Length", std::to_string(filesize));
+
+            // SetCode(302);
+            // SetHeader("Location", "http://8.137.19.140:8080/404.html");
+            // return true;
         }
         else
         {
@@ -173,6 +218,11 @@ public:
             SetHeader("Content-Length", std::to_string(filesize));
         }
         return true;
+    }
+
+    void SetText(const std::string &t)
+    {
+        _text = t;
     }
 
     bool Deserialize(std::string &reqstr)
@@ -196,6 +246,8 @@ public:
     std::string _targetfile;
 };
 
+using http_func_t = std::function<void(HttpRequest &req, HttpResponse &resp)>;
+
 class Http
 {
 public:
@@ -213,15 +265,35 @@ public:
             std::cout << "##########################" << std::endl;
             std::cout << httpreqstr;
             std::cout << "##########################" << std::endl;
-            
+
             HttpRequest req;
-            req.Deserialize(httpreqstr);
             HttpResponse resp;
-            resp.SetTargetFile(req.Uri());
-            if (resp.MakeResponse())
+            req.Deserialize(httpreqstr);
+            if (req.isInteract())
             {
-                std::string response_str = resp.Serialize();
-                sock->Send(response_str);
+                // _uri: ./wwwroot/login
+                if (_route.find(req.Uri()) == _route.end())
+                {
+                    // SetCode(302)
+                }
+                else
+                {
+                    _route[req.Uri()](req, resp);
+                    std::string response_str = resp.Serialize();
+                    sock->Send(response_str);
+                }
+            }
+            else
+            {
+                // 我这里有一个疑惑：req.Uri()已经能获取目标网页，为什么还要调用SetTargetFile？
+                // 答案是：能实现请求与应答的解耦
+                resp.SetTargetFile(req.Uri()); // 判断要获取哪个网页
+
+                if (resp.MakeResponse()) // 构建响应报文
+                {
+                    std::string response_str = resp.Serialize();
+                    sock->Send(response_str);
+                }
             }
         }
 
@@ -250,10 +322,21 @@ public:
                      { this->HandlerHttpRequest(sock, client); });
     }
 
+    void RegisterService(const std::string name, http_func_t h)
+    {
+        std::string key = webroot + name; // ./wwwroot/login
+        auto iter = _route.find(key);
+        if (iter == _route.end())
+        {
+            _route.insert(std::make_pair(key, h));
+        }
+    }
+
     ~Http()
     {
     }
 
 private:
     std::unique_ptr<TcpServer> tsvrp;
+    std::unordered_map<std::string, http_func_t> _route;
 };
--- http/Main.cc
+++ http/Main.cc
@@ -1,5 +1,42 @@
 #include "Http.hpp"
 
+void Login(HttpRequest &req, HttpResponse &resp)
+{
+    LOG(LogLevel::DEBUG) << req.Args() << ", 我们成功进入到了处理数据的逻辑";
+    std::string text = "hello: " + req.Args();
+
+    // 登录认证
+
+    resp.SetCode(200);
+    resp.SetHeader("Content-Type", "text/plain");
+    resp.SetHeader("Content-Length", std::to_string(text.size()));
+    resp.SetText(text);
+}
+void Register(HttpRequest &req, HttpResponse &resp)
+{
+    LOG(LogLevel::DEBUG) << req.Args() << ", 我们成功进入到了处理数据的逻辑";
+    std::string text = "hello: " + req.Args();
+
+    resp.SetCode(200);
+    resp.SetHeader("Content-Type", "text/plain");
+    resp.SetHeader("Content-Length", std::to_string(text.size()));
+    resp.SetText(text);
+}
+void VipCheck(HttpRequest &req, HttpResponse &resp)
+{
+    LOG(LogLevel::DEBUG) << req.Args() << ", 我们成功进入到了处理数据的逻辑";
+    std::string text = "hello: " + req.Args();
+    resp.SetCode(200);
+    resp.SetHeader("Content-Type", "text/plain");
+    resp.SetHeader("Content-Length", std::to_string(text.size()));
+    resp.SetText(text);
+}
+
+void Search(HttpRequest &req, HttpResponse &resp)
+{
+}
+
+// http port
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -9,8 +46,13 @@ int main(int argc, char *argv[])
     }
     uint16_t port = std::stoi(argv[1]);
 
-    std::unique_ptr<Http> httpptr = std::make_unique<Http>(port);
-    httpptr->Start();
-    
+    std::unique_ptr<Http> httpsvr = std::make_unique<Http>(port);
+    httpsvr->RegisterService("/login", Login); //
+    httpsvr->RegisterService("/register", Register);
+    httpsvr->RegisterService("/vip_check", VipCheck);
+    httpsvr->RegisterService("/s", Search);
+    // httpsvr->RegisterService("/", Login);
+
+    httpsvr->Start();
     return 0;
 }
